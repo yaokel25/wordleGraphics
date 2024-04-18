1e1209903706e80b09d52d4ffcd99731bbf242a5
yaokel25 <114490360+yaokel25@users.noreply.github.com>
2024-04-18 13:09:16 -0400

    Update Tester.java-started to get random word from longList.txt (2,502 words)

--- Tester.java
+++ Tester.java
@@ -4,6 +4,7 @@ public class Tester{
     public static void main(String args[]){
         String input = "";
         Scanner scan = new Scanner(System.in);
+        String wordGuess = 
         Wordle w1 = new Wordle("fives");
         for(int i = 0; i < 5; i++){
             System.out.println("Enter your guess: ");
