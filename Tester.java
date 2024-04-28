c3fbfc25d0055e73025dcbb5bbacaed9247f54b5
Keliang <yaokel25@7hills.org>
2024-04-28 16:34:52 -0400

    finished version

--- Tester.java
+++ Tester.java
@@ -1,6 +1,7 @@
 import doodlepad.*;
 import java.io.File;
 import java.util.Scanner;
+
 import java.io.FileNotFoundException;
 public class Tester{
     public static String [] myArray = new String[2502];
@@ -28,24 +29,16 @@ public class Tester{
         }*/
 
         return myArray[randomWord];
-         
-       
-    }
+}
     public static void main(String args[]){
-        //boolean isWord = false;
-        //String input = "";
+        Scanner scan = new Scanner(System.in);
+        boolean isWord = false;
         String inputWord = readFile();
+        String input = "";
         System.out.print(inputWord);
-        //Scanner scan = new Scanner(System.in);
-        Pad inScreen = new Pad("Instructions", 1000, 1000);
-        Text instructions = new Text("Welcome to Wordle!", 335, 260, 35);
-        Text instructions1 = new Text("You have 6 tries to guess a five letter word. Enter your guesses into the terminal.", 55, 400, 25);
-        Text instructions2 = new Text("Letter combinations that are not words or are not 5 letters long are not valid inputs.", 45, 460, 25);
-        Text instructions3 = new Text("If you did not input a valid word, you will be reprompted to enter your guess.", 80, 520, 25);
-        PushButton instructionButton = new PushButton(inputWord);
-        //Text instructions4 = new Text("Press enter to continue", 400, 640, 25);
-        //Wordle w1 = new Wordle(inputWord);
-        /*for(int i = 0; i < 6; i++){
+        Wordle w1 = new Wordle(inputWord);
+        w1.drawInstructions();
+            for(int i = 0; i < 6; i++){
             do{
             System.out.println("Enter your guess: ");
             input = scan.nextLine();
@@ -74,77 +67,7 @@ public class Tester{
         if(!w1.getCheckString()){
             w1.drawDefeat();
             System.out.print("You didn't guess it");
-        }*/
-        
-    }
-}
-class PushButton {
-    private RoundRect shpButton;    // Button Shape
-    private boolean isOn;           // Button state
-    private boolean isWord = false;
-    private String input = "";
-
-    public PushButton(String inputWord) {
-        // Create button shape and initialize
-        shpButton = new RoundRect(345, 610, 300, 50, 20, 20);
-        shpButton.setFillColor(200);
-        Text instructions4 = new Text("Press here to continue", 370, 625, 25);
-        input = inputWord;
-        // Starts off
-        isOn = false;
-
-        // Set button click event handler method
-        //shpButton.setMousePressedHandler( this::onPressed );
-        instructions4.setMousePressedHandler( this::onPressed );
-    }
-
-    private void onPressed(Shape shp, double x, double y, int button) {
-        // Toggle button state
-        Scanner scan = new Scanner(System.in);
-        isOn = !isOn;
-        Tester t1 = new Tester();
-
-        // Set button fill color based on state
-        if (isOn) {
-            Wordle w1 = new Wordle(input);
-            for(int i = 0; i < 6; i++){
-            do{
-            System.out.println("Enter your guess: ");
-            input = scan.nextLine();
-            input = input.toLowerCase();
-            //System.out.println("BEFORE FOR LOOP"); checking code
-            for(int r = 0; r < 2501; r++){
-                if(input.equals(t1.myArray[r])){
-                    //System.out.println("IF STATEMENT"); checking code
-                    isWord = true;
-                    //System.out.println("TRUE"); checking code
-                    break;
-                }
-                else{
-                    isWord = false;
-                    //System.out.println("FALSE"); checking code
-                }
-            }
-            }
-            while((input.length() != 5) || (isWord == false));
-            //System.out.println("WHILE LOOP DONE"); checking code
-            w1.checkWord(input, i);
-            if(w1.getCheckString()){
-                break;
-            }
-        }
-        if(!w1.getCheckString()){
-            w1.drawDefeat();
-            System.out.print("You didn't guess it");
         }
     }
-            //shpButton.setFillColor(0, 255, 0);
-         else {
-            //shpButton.setFillColor(200);
-        }
-    }
-
-            
-    
 }
 
--- Wordle.java
+++ Wordle.java
@@ -1,15 +1,45 @@
 import doodlepad.*;
 import java.io.File;
 import java.util.Scanner;
+
+
+
 import java.io.FileNotFoundException;
 public class Wordle extends Pad{
     private String key1;
     private boolean checkString = false;
+    private Text instructions;
+    private Text instructions1;
+    private Text instructions2;
+    private Text instructions3;
+    private Text instructions4;
+    private RoundRect shpButton;
     public Wordle(String key){
         super("Wordle", 1000, 1200);
         key1 = key;
     }
-    
+    public void drawInstructions(){
+        instructions = new Text("Welcome to Wordle!", 335, 260, 35);
+        instructions1 = new Text("You have 6 tries to guess a five letter word. Enter your guesses into the terminal.", 55, 400, 25);
+        instructions2 = new Text("Letter combinations that are not words or are not 5 letters long are not valid inputs.", 45, 460, 25);
+        instructions3 = new Text("If you did not input a valid word, you will be reprompted to enter your guess.", 80, 520, 25);
+        shpButton = new RoundRect(345, 610, 300, 50, 20, 20);
+        shpButton.setFillColor(200);
+        instructions4 = new Text("Press here to continue", 370, 625, 25);
+        instructions4.setMouseClickedHandler( this::hidePiece );
+        shpButton.setMouseClickedHandler( this::hidePiece );
+
+
+    }
+    public void hidePiece(Shape shp, double x, double y, int b){
+        instructions.setVisible(false);
+        instructions1.setVisible(false);
+        instructions2.setVisible(false);
+        instructions3.setVisible(false);
+        instructions4.setVisible(false);
+        shpButton.setVisible(false);
+        //shp.setVisible(false);
+    }
     public void drawVictory(){
         Rectangle backgroundV = new Rectangle(0,0,1000,1200);
         backgroundV.setFillColor(165,227,161);
@@ -20,9 +50,35 @@ public class Wordle extends Pad{
         Text defeatText = new Text("YOU DIDN'T GUESS IT", 180, 500, 55);
         Text victoryText1 = new Text("WORD: \"" + key1 + "\"", 320, 600, 55);
     }
+    public void printRed(String input, int i, int numTry, boolean checkLetter){
+        System.out.println("Red ");
+        Rectangle box = new Rectangle(i*200, numTry*200, 200, 200);
+        box.setFillColor(255,0,0);
+        Text letter1 = new Text(String.valueOf(input.charAt(i)), i*200 + 75, numTry*200 + 75, 100);
+        System.out.println("Printed red letter");
+
+    }
+    public void printYellow(String input, int i, int numTry, boolean checkLetter){
+        System.out.println("Yellow ");
+        Rectangle box = new Rectangle(i*200, numTry*200, 200, 200);
+        box.setFillColor(255,190,0);
+        Text letter1 = new Text(String.valueOf(input.charAt(i)), i*200 + 75, numTry*200 + 75, 100);
+        System.out.println("Printed yellow letter");
+        checkLetter = false;
+
+    }
+    public void printGreen(String input, int i, int numTry, boolean checkLetter){
+        System.out.println("Green ");
+        Rectangle box = new Rectangle(i*200, numTry*200, 200, 200);
+        box.setFillColor(0,255,0);
+        Text letter1 = new Text(String.valueOf(input.charAt(i)), i*200 + 75, numTry*200 + 75, 100);
+        System.out.println("Printed green letter");
+
+    }
     public void checkWord(String input, int numTry){
         boolean checkLetter = false;
         if(input.equals(key1)){
+            System.out.println("input = key1");
             drawVictory();
             Text victoryText = new Text("YOU GUESSED IT!", 245, 500, 55);
             Text victoryText1 = new Text("Word: \"" + key1 + "\"", 320, 600, 55);
@@ -30,12 +86,10 @@ public class Wordle extends Pad{
             checkString = true;
         }
         else{
+            System.out.println("Else 1");
             for(int i = 0; i < 5; i++){
                 if(input.charAt(i) == key1.charAt(i)){
-                    System.out.println("Green ");
-                    Rectangle box = new Rectangle(i*200, numTry*200, 200, 200);
-                    box.setFillColor(0,255,0);
-                    Text letter1 = new Text(String.valueOf(input.charAt(i)), i*200 + 75, numTry*200 + 75, 100);
+                   printGreen(input, i, numTry, checkLetter); 
                 }
                 else{
                     for(int y = 0; y < 5; y++){
@@ -44,17 +98,10 @@ public class Wordle extends Pad{
                         }
                     }
                     if(checkLetter){
-                        System.out.println("Yellow ");
-                        Rectangle box = new Rectangle(i*200, numTry*200, 200, 200);
-                        box.setFillColor(255,190,0);
-                        Text letter1 = new Text(String.valueOf(input.charAt(i)), i*200 + 75, numTry*200 + 75, 100);
-                        checkLetter = false;
+                        printYellow(input, i, numTry, checkLetter);
                     }
                     else{
-                        System.out.println("Red ");
-                        Rectangle box = new Rectangle(i*200, numTry*200, 200, 200);
-                        box.setFillColor(255,0,0);
-                        Text letter1 = new Text(String.valueOf(input.charAt(i)), i*200 + 75, numTry*200 + 75, 100);
+                        printRed(input, i, numTry, checkLetter);
                     }
                 }
             }
