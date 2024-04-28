c77637f7f42c3a39f5fabcd797f20e42c7470665
Keliang <yaokel25@7hills.org>
2024-04-28 16:48:24 -0400

    added coments

--- Tester.java
+++ Tester.java
@@ -1,12 +1,11 @@
 import doodlepad.*;
 import java.io.File;
 import java.util.Scanner;
-
 import java.io.FileNotFoundException;
 public class Tester{
     public static String [] myArray = new String[2502];
     public static String readFile(){
-        //String [] wordList = new String[2502];
+        //String [] wordList = new String[2502];-struggle: originally tried to copy list onto another string
         try {
             File myObj = new File("longlist.txt");
             Scanner myReader = new Scanner(myObj);
@@ -19,27 +18,27 @@ public class Tester{
             e.printStackTrace();
         }
         
-        int numWords = myArray.length;
+        int numWords = myArray.length;//getting number of words in longList
         //System.out.println("numWords: " + numWords);
-        int randomWord = 0;
-        randomWord = (int)(Math.random()*numWords);
+        int randomNum = 0;//declaring and initializing index for random word
+        randomNum = (int)(Math.random()*numWords);//picking a random number from 0 to 2502
         //System.out.println(randomWord);
         /*for(int t = 0; t < 2502; t++){
             System.out.println(myArray[t]);
         }*/
 
-        return myArray[randomWord];
+        return myArray[randomNum];//returning the word at index randomNum (a random word)
 }
     public static void main(String args[]){
         Scanner scan = new Scanner(System.in);
         boolean isWord = false;
         String inputWord = readFile();
         String input = "";
-        System.out.print(inputWord);
+        //System.out.print(inputWord); check code
         Wordle w1 = new Wordle(inputWord);
-        w1.drawInstructions();
+        w1.drawInstructions();//display instructions
             for(int i = 0; i < 6; i++){
-            do{
+            do{//keep prompting for guesses if the user input is not 5 letter or a word from the longList 
             System.out.println("Enter your guess: ");
             input = scan.nextLine();
             input = input.toLowerCase();
@@ -59,12 +58,12 @@ public class Tester{
             }
             while((input.length() != 5) || (isWord == false));
             //System.out.println("WHILE LOOP DONE"); checking code
-            w1.checkWord(input, i);
-            if(w1.getCheckString()){
-                break;
+            w1.checkWord(input, i);//checks user gues and prints out the guess and colored boxes
+            if(w1.getCheckString()){//checks if user guesses the word
+                break;//stop the game if user guesses the correct word
             }
         }
-        if(!w1.getCheckString()){
+        if(!w1.getCheckString()){//if user still has not guesses the word, then draw the defeat screen
             w1.drawDefeat();
             System.out.print("You didn't guess it");
         }
--- Wordle.java
+++ Wordle.java
@@ -14,11 +14,12 @@ public class Wordle extends Pad{
     private Text instructions3;
     private Text instructions4;
     private RoundRect shpButton;
+    private int numLetters = 5;
     public Wordle(String key){
-        super("Wordle", 1000, 1200);
+        super("Wordle", 1000, 1200);//this pad size only works on pcs it is too big for my macbook air computer screen
         key1 = key;
     }
-    public void drawInstructions(){
+    public void drawInstructions(){//draw instructions
         instructions = new Text("Welcome to Wordle!", 335, 260, 35);
         instructions1 = new Text("You have 6 tries to guess a five letter word. Enter your guesses into the terminal.", 55, 400, 25);
         instructions2 = new Text("Letter combinations that are not words or are not 5 letters long are not valid inputs.", 45, 460, 25);
@@ -26,31 +27,31 @@ public class Wordle extends Pad{
         shpButton = new RoundRect(345, 610, 300, 50, 20, 20);
         shpButton.setFillColor(200);
         instructions4 = new Text("Press here to continue", 370, 625, 25);
-        instructions4.setMouseClickedHandler( this::hidePiece );
-        shpButton.setMouseClickedHandler( this::hidePiece );
+        instructions4.setMouseClickedHandler( this::hidePiece );//struggle: originally had this in tester class and then switched it to a button but neither of them worked so I put them in this class so I don't have to worry about static and void methods
+        shpButton.setMouseClickedHandler( this::hidePiece );//struggle: originally did not have this but realized that both button and text have to to have to for it to work
 
 
     }
-    public void hidePiece(Shape shp, double x, double y, int b){
+    public void hidePiece(Shape shp, double x, double y, int b){//method to hide instructions after button is clicked
         instructions.setVisible(false);
         instructions1.setVisible(false);
         instructions2.setVisible(false);
         instructions3.setVisible(false);
         instructions4.setVisible(false);
         shpButton.setVisible(false);
-        //shp.setVisible(false);
+        //shp.setVisible(false);-original code that only hid the button that is being pressed
     }
-    public void drawVictory(){
+    public void drawVictory(){//drawing screen is user currectly guesses word
         Rectangle backgroundV = new Rectangle(0,0,1000,1200);
         backgroundV.setFillColor(165,227,161);
     }
-    public void drawDefeat(){
+    public void drawDefeat(){//draws screen if user does not guess the word within 6 guesses
         Rectangle background1 = new Rectangle(0,0,1000,1200);
         background1.setFillColor(255,0,0);
         Text defeatText = new Text("YOU DIDN'T GUESS IT", 180, 500, 55);
         Text victoryText1 = new Text("WORD: \"" + key1 + "\"", 320, 600, 55);
     }
-    public void printRed(String input, int i, int numTry, boolean checkLetter){
+    public void printRed(String input, int i, int numTry, boolean checkLetter){//prints letter and a red square if user guesses a letter that is not in the word, used in checkWord method
         System.out.println("Red ");
         Rectangle box = new Rectangle(i*200, numTry*200, 200, 200);
         box.setFillColor(255,0,0);
@@ -58,7 +59,7 @@ public class Wordle extends Pad{
         System.out.println("Printed red letter");
 
     }
-    public void printYellow(String input, int i, int numTry, boolean checkLetter){
+    public void printYellow(String input, int i, int numTry, boolean checkLetter){//prints a yellow box and the letter if user guesses the right letter but in the wrong position, used in checkWord method
         System.out.println("Yellow ");
         Rectangle box = new Rectangle(i*200, numTry*200, 200, 200);
         box.setFillColor(255,190,0);
@@ -67,7 +68,7 @@ public class Wordle extends Pad{
         checkLetter = false;
 
     }
-    public void printGreen(String input, int i, int numTry, boolean checkLetter){
+    public void printGreen(String input, int i, int numTry, boolean checkLetter){//prints green box and the letter if user guesses right letter in the right position, used in checkWord method
         System.out.println("Green ");
         Rectangle box = new Rectangle(i*200, numTry*200, 200, 200);
         box.setFillColor(0,255,0);
@@ -75,9 +76,9 @@ public class Wordle extends Pad{
         System.out.println("Printed green letter");
 
     }
-    public void checkWord(String input, int numTry){
+    public void checkWord(String input, int numTry){//takes the user's guesses as input and checks every letter and prints out letter and a colored box if it is right, wrong position, or wrong
         boolean checkLetter = false;
-        if(input.equals(key1)){
+        if(input.equals(key1)){//if input equals the word
             System.out.println("input = key1");
             drawVictory();
             Text victoryText = new Text("YOU GUESSED IT!", 245, 500, 55);
@@ -86,29 +87,29 @@ public class Wordle extends Pad{
             checkString = true;
         }
         else{
-            System.out.println("Else 1");
-            for(int i = 0; i < 5; i++){
-                if(input.charAt(i) == key1.charAt(i)){
-                   printGreen(input, i, numTry, checkLetter); 
+            //System.out.println("Else 1");-check code
+            for(int i = 0; i < numLetters; i++){//iterates through all letter in word
+                if(input.charAt(i) == key1.charAt(i)){//if it is the right letter in the right position
+                   printGreen(input, i, numTry, checkLetter); //print green box and letter
                 }
                 else{
-                    for(int y = 0; y < 5; y++){
+                    for(int y = 0; y < numLetters; y++){//if it is the right letter but wrong box, then checkLetter is true
                         if(input.charAt(i) == key1.charAt(y)){
                             checkLetter = true;
                         }
                     }
                     if(checkLetter){
-                        printYellow(input, i, numTry, checkLetter);
+                        printYellow(input, i, numTry, checkLetter);//prints yellow box and letter
                     }
                     else{
-                        printRed(input, i, numTry, checkLetter);
+                        printRed(input, i, numTry, checkLetter);//otherwise, print red box and letter
                     }
                 }
             }
         }
     }
     
-    public boolean getCheckString(){
+    public boolean getCheckString(){//return true if user guess is correct, false if it is wrong
         return checkString;
     }
 
